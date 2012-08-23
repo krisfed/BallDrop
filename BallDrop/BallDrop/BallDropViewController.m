d9f2ed234d9f725b49026613a99044c2333d9e97
Kris Fedorenko <kfedoren@gmail.com>
2012-08-22 21:21:13 -0400

    added tap gesture and buttons (fixed conflict between them)

--- BallDrop/BallDrop/BallDropViewController.m
+++ BallDrop/BallDrop/BallDropViewController.m
@@ -17,7 +17,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UITapGestureRecognizer *tap =[[UITapGestureRecognizer alloc] initWithTarget: self action:@selector(handleTap:)];
+    tap.delegate = self;
+    [self.view addGestureRecognizer:tap];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)handleTap:(UITapGestureRecognizer *) tap
+{
+    NSLog(@"Tap!");
+}
+
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *) touch 
+{
+    return ([touch locationInView:self.view].y < self.view.bounds.size.height - 100);
+}
+
+
+- (IBAction)newFilePressed:(UIButton *)sender {
+    NSLog(@"new file");
+}
+
+- (IBAction)saveFilePressed:(UIButton *)sender {
+}
+
+- (IBAction)loadFilePressed:(UIButton *)sender {
+}
+
+- (IBAction)newBallSourcePressed:(UIButton *)sender {
+}
+
+- (IBAction)playStopPressed:(UIButton *)sender {
 }
 
 - (void)viewDidUnload
